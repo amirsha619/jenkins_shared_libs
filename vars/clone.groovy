7a978c8b79a6fd7250d0a02056b8bfa28fbbf58d
Amirsha Dudgaon <amirs035@gmail.com>
2025-02-25 16:15:18 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -2,8 +2,8 @@ def call(String url, String branch ,String cred_id)
 {
   echo 'This is the stage to clone the SCM repo from shared lib'
 
-  git branch: ${branch}, 
-  credentialsId: ${cred_id},  // Ensure this credential ID exists in Jenkins
-  url: ${url}
+  git branch: "${branch}", 
+  credentialsId: "${cred_id}",  // Ensure this credential ID exists in Jenkins
+  url: "${url}"
   echo 'This is the stage to clone the SCM repo from shared lib'
 }    
