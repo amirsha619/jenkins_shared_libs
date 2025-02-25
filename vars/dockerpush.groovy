560483bdd38beaad511c010a9d404f5314276117
Amirsha Dudgaon <amirs035@gmail.com>
2025-02-25 16:33:53 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,8 +1,8 @@
 def call(String credId, String DOCKER_IMAGE,String IMAGE_TAG)
 {
   echo 'Pushing Docker image...'
-  withDockerRegistry([credentialsId: credId, url: '']) {
-  sh 'docker push DOCKER_IMAGE:IMAGE_TAG'
+  withDockerRegistry([credentialsId: ${credId}, url: '']) {
+  sh 'docker push ${DOCKER_IMAGE}:${IMAGE_TAG}'
   }
   echo 'Docker image pushed successfully!'
 }
