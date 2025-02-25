f6e4318c6b2215303689e1135abce8008beae7d8
Amirsha Dudgaon <amirs035@gmail.com>
2025-02-25 15:34:26 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-echo  " Hello jenkins "
+echo  " Hello jenkins  This is my first shared library in jenkins, And i m going to use this in as many pipelines as i can. Thus providing The  resuability."
 }
